6cc8349ba095367907a0798dbc077f60817b0840
“Fahmi-ssy” <fahmihariadi896@gmail.com>
2024-09-12 15:48:12 +0700

    selesai Seeder

---
+++ controllers/controller.js
@@ -0,0 +1,15 @@
+
+
+
+class Controller {
+    static async displayAllInvestmen(req,res){
+        try {
+            console.log('aaaaaaaaaa');
+            
+        } catch (error) {
+            res.send(error)
+        }
+    }
+}
+
+module.exports = Controller
